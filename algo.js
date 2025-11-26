d1e2ac9e689e99ddbcd45ea3e27c99f608787ec0
AsokTamang <ashoktmg2058@gmail.com>
2025-11-26 09:09:02 -0500

    switch method

--- algo.js
+++ algo.js
@@ -1,9 +1,45 @@
-const playerGuess = 3
-const correctAnswer = 6
+const playerGuess = 3;
+const correctAnswer = 6;
 
 /*
 Challenge 
 1. Refactor the if else statement to use a ternary operator.
 */
 
-let message =playerGuess==correctAnswer? 'Correct!':'Wrong!'
+let message = playerGuess == correctAnswer ? "Correct!" : "Wrong!";
+
+//switch statements
+/**
+ * Price List
+ * Coffee $2
+ * Sandwiches $5
+ * Salad $4
+ * Lemon Cake $3
+ */
+
+// For example: "You selected Salad. That will be $4"
+
+function selectItem(item) {
+  let price = 0;
+  /*
+Challenge:
+1.  Add the remaining price list items as cases.
+*/
+  switch (item) {
+    case "coffee":
+      price = 2;
+      break;
+    case "sandwiches":
+      price = 5;
+      break;
+    case "salad":
+      price = 4;
+      break;
+    case "lemon cake":
+      price = 3;
+      break;
+  }
+  return `You selected ${item}. That will be $${price}`;
+}
+
+console.log(selectItem("biscuits"));
